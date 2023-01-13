1b6e1051c92816b0d7aa321a41f68e64c1ab3178
prernanimje <prernanimje5@gmail.com>
2023-01-13 17:03:54 +0530

    Positive number count in Array

---
+++ src/main/java/com/bootcoding/dsa/Array/positiveNumberCount.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa.Array;
+
+public class positiveNumberCount {
+    public static int maximumCount(int[] num) {
+
+        int pos = 0;
+        int neg = 0;
+        for (int j : num) {
+            if (j > 0) {
+                pos++;
+
+            } else if (j < 0) {
+                neg++;
+
+            }
+        }
+            return Math.max(pos, neg);
+        }
+
+
+    public static void main(String[] args) {
+        int[]num = {-1,1,2,3,-5,4};
+     
+     int res= maximumCount(num);
+        System.out.println(res);
+    }
+}
