557482a74656685f5484d808298c4c2f0e945678
prernanimje <prernanimje5@gmail.com>
2023-01-16 11:29:28 +0530

    Odd program in LinkedList

---
+++ src/main/java/com/bootcoding/dsa/linkedList/odd.java
@@ -0,0 +1,37 @@
+package com.bootcoding.dsa.linkedList;
+public class odd {
+        public static Node creatLinkedList(){
+            Node firstNode = new Node(22);
+            Node secondNode = new Node(4);
+            Node thirdNode = new Node(2);
+            Node fourthNode = new Node(7);
+
+            Node head= firstNode;
+            firstNode.next = secondNode;
+            secondNode.next=thirdNode;
+            thirdNode.next=fourthNode;
+            fourthNode.next=null;
+            return firstNode;
+        }
+
+        public static void oddNumber(Node head){
+            Node temp = head;
+            while(temp!= null){
+                if (temp.data%2 != 0){
+                    System.out.println(temp.data + " ");
+
+                }
+                temp=temp.next;
+            }
+            System.out.println();
+
+        }
+
+        public static void main(String[] args) {
+            Node head =creatLinkedList();
+            oddNumber(head);
+        }
+
+    }
+
+
