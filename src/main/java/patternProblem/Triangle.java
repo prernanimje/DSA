cfa3db879bb496fa4ccaf110524d648a9979ae37
prernanimje <prernanimje5@gmail.com>
2023-01-16 10:54:10 +0530

    Pattern Program

---
+++ src/main/java/patternProblem/Triangle.java
@@ -0,0 +1,15 @@
+package patternProblem;
+
+public class Triangle {
+
+
+    public static void main(String[] args) {
+        int i, j,n = 4;
+        for (i = 0; i <=n; i++) {
+            for (j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
