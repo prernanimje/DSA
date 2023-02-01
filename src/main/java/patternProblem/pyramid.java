0783447160d71d6f701e1b5e26efec5dc44def4d
prernanimje <prernanimje5@gmail.com>
2023-02-01 22:10:59 +0530

    pyramid

---
+++ src/main/java/patternProblem/pyramid.java
@@ -0,0 +1,26 @@
+package patternProblem;
+
+public class pyramid {
+    public static void Print(int rows){
+           int k=0 ;
+
+            for (int i = 1; i <= rows; ++i, k = 0) {
+                for (int space = 1; space <= rows - i; ++space) {
+                    System.out.print("  ");
+                }
+
+                while (k != 2 * i - 1) {
+                    System.out.print("* ");
+                    ++k;
+                }
+
+                System.out.println();
+            }
+        }
+
+    public static void main(String[] args) {
+        int rows=5;
+        Print(rows);
+    }
+    }
+
