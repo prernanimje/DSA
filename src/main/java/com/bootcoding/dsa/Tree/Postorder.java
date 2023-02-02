9cd24414627e39346f81f0714b514fcf900df88a
prernanimje <prernanimje5@gmail.com>
2023-02-02 10:57:10 +0530

    postorder in tree

---
+++ src/main/java/com/bootcoding/dsa/Tree/Postorder.java
@@ -0,0 +1,47 @@
+package com.bootcoding.dsa.Tree;
+
+public class Postorder {
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+        Node n5 = new Node(5);
+        Node n6 = new Node(6);
+        Node n7 = new Node(7);
+        Node n8 = new Node(8);
+        Node n9 = new Node(9);
+        Node n10 = new Node(10);
+        Node n11 = new Node(11);
+        Node n12 = new Node(12);
+        Node n13 = new Node(13);
+        Node n14 = new Node(14);
+        Node n15 = new Node(15);
+
+        root.left = n2;
+        root.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n3.right = n7;
+        n6.left = n12;
+        n6.right = n13;
+        n7.left = n14;
+        n7.right = n15;
+        n4.left = n8;
+        n4.right = n9;
+        n5.left = n10;
+        n5.right = n11;
+
+        postorder(root);
+    }
+    public static void postorder(Node root){
+        if(root==null){
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data);
+    }
+    }
