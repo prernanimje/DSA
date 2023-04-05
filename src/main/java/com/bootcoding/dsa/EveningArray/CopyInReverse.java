da87a29ddafbfec8bd0d5e5c30c39e68c94cee95
prernanimje <prernanimje5@gmail.com>
2023-04-05 20:16:07 +0530

    Copy elements in reverse

---
+++ src/main/java/com/bootcoding/dsa/EveningArray/CopyInReverse.java
@@ -0,0 +1,15 @@
+package com.bootcoding.dsa.EveningArray;
+
+public class CopyInReverse {
+    public static void main(String[] args) {
+        int[] old = {20, 25, 1, 0, 2, 5, 4};
+        int[] new1 = new int[old.length];
+        for (int i=old.length-1;i>=0;--i){
+            new1[i]=old[i];
+
+        }
+        for (int i=new1.length-1;i>=0;--i){
+            System.out.println(new1[i]);
+        }
+    }
+}
