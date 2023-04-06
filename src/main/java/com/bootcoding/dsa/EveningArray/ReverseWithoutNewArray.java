6e0c4afa4508482cab94dfee5b6d20575173cf15
prernanimje <prernanimje5@gmail.com>
2023-04-06 22:01:40 +0530

    Reverse array without using new array .

---
+++ src/main/java/com/bootcoding/dsa/EveningArray/ReverseWithoutNewArray.java
@@ -0,0 +1,10 @@
+package com.bootcoding.dsa.EveningArray;
+
+public class ReverseWithoutNewArray {
+    public static void main(String[] args) {
+        int[]array={1,2,3,4,5};
+        for (int i=array.length-1;i>=0;--i){
+            System.out.println(array[i]);
+        }
+    }
+}
