1fa5f28dc32f2497d2713062de088077adb01adc
prernanimje <prernanimje5@gmail.com>
2023-04-12 19:50:12 +0530

    String

---
+++ src/main/java/com/bootcoding/dsa/String/FindCharIndex.java
@@ -0,0 +1,11 @@
+package com.bootcoding.dsa.String;
+
+public class FindCharIndex {
+    public static void main(String[] args) {
+        String str="Programming";
+        int r1=str.indexOf('a');
+        System.out.println(r1);
+        int r2=str.lastIndexOf('g');
+        System.out.println(r2);
+    }
+}
