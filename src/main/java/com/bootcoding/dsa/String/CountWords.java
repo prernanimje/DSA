7fa855256a9f8ab9ce096ef157fe0a15b1d3deb1
prernanimje <prernanimje5@gmail.com>
2023-04-13 19:17:29 +0530

    Substring

---
+++ src/main/java/com/bootcoding/dsa/String/CountWords.java
@@ -0,0 +1,10 @@
+package com.bootcoding.dsa.String;
+
+public class CountWords {
+    public static void main(String[] args) {
+        String str="Java is platform Independent";
+        System.out.println(str.length());
+       // String[]token=str.split("");
+
+    }
+}
