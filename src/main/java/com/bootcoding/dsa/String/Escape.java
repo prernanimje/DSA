5b2592c6b33151b29f90bd424709c698232fb2d2
prernanimje <prernanimje5@gmail.com>
2023-04-14 19:33:48 +0530

    practice

---
+++ src/main/java/com/bootcoding/dsa/String/Escape.java
@@ -0,0 +1,9 @@
+package com.bootcoding.dsa.String;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("I'm alright");
+        System.out.println("I am software Engineer in\"Google\" ");
+        System.out.println("Today is 14\\04\\2023");
+    }
+}
