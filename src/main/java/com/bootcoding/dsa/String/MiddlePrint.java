6c6dc974aeaaeb381f416182ce424a8c62759b3e
prernanimje <prernanimje5@gmail.com>
2023-04-14 19:31:36 +0530

    practice

---
+++ src/main/java/com/bootcoding/dsa/String/MiddlePrint.java
@@ -0,0 +1,10 @@
+package com.bootcoding.dsa.String;
+
+public class MiddlePrint {
+    public static void main(String[] args) {
+        String str="Programming";
+        int len=str.length();
+        char ch=str.charAt(len/2);
+        System.out.println(ch);
+    }
+}
