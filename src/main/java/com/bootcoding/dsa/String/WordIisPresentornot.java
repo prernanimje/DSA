798fbb078600f1fb1a2231e0cdd8d1e0eda5e21b
prernanimje <prernanimje5@gmail.com>
2023-04-14 19:32:21 +0530

    practice

---
+++ src/main/java/com/bootcoding/dsa/String/WordIisPresentornot.java
@@ -0,0 +1,11 @@
+package com.bootcoding.dsa.String;
+
+public class WordIisPresentornot {
+    public static void main(String[] args) {
+        String str ="Programming";
+        String word="ram";
+        if (str.contains(word)){
+            System.out.println("ram exists");
+        }
+    }
+}
