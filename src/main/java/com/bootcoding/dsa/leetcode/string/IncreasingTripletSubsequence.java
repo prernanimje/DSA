3e836b05242688f924c88256f3baa81e6c731f31
prernanimje <prernanimje5@gmail.com>
2024-11-11 21:57:13 +0530

    334. Increasing Triplet Subsequence

---
+++ src/main/java/com/bootcoding/dsa/leetcode/string/IncreasingTripletSubsequence.java
@@ -0,0 +1,27 @@
+package com.bootcoding.dsa.leetcode.string;
+
+public class IncreasingTripletSubsequence {
+    public static boolean increasingTriplet(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return false;
+        }
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= a) {
+                a = num;
+            } else if (num <= b) {
+                b = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
